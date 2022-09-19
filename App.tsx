f74c543dd79b0b5b755db976e895de6f83ef6598
Henry Moulton <henry.moulton@yonderteam.co>
2022-09-19 11:00:41 +0100

    feat: add screenshot

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, useWindowDimensions, View} from 'react-native';
 
 export default function App() {
+  const { height, width} = useWindowDimensions();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./screenshots/home-discovery.jpeg")} resizeMode={"contain"} style={{ width, height}}/>
     </View>
   );
 }
