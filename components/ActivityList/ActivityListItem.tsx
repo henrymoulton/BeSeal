5032373c00383e1214efa668fd4e109afb5c78b6
Henry Moulton <henry.moulton@yonderteam.co>
2022-09-19 13:07:01 +0100

    feat: add changing seal

--- components/ActivityList/ActivityListItem.tsx
+++ components/ActivityList/ActivityListItem.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
+import { useState } from "react";
 
-import { Image, Text, useWindowDimensions, View } from "react-native";
+import {
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import { ActivityListItemHeader } from "./ActivityListItemHeader";
 import { EmojiPicker } from "./EmojiPicker";
 
@@ -9,44 +15,39 @@ type ActivityListItemProps = {};
 export const ActivityListItem = ({}: ActivityListItemProps) => {
   const { width } = useWindowDimensions();
 
+  const frontSrc = require("../../assets/images/front.jpg");
+  const backSrc = require("../../assets/images/back.jpg");
+
+  const [srcs, setSrcs] = useState({
+    thumbnailSrc: frontSrc,
+    mainSrc: backSrc,
+  });
+
+  const handlePressImages = () => {
+    setSrcs((prevState) => ({
+      mainSrc: prevState.thumbnailSrc,
+      thumbnailSrc: prevState.mainSrc,
+    }));
+  };
+
   return (
     <View>
       <ActivityListItemHeader />
-      <View
-        style={{
-          backgroundColor: "papayawhip",
-          borderColor: "white",
-          borderRadius: 12,
-          borderWidth: 4,
-          margin: 8,
-          height: width * 1.3,
-          width: width - 16,
-          overflow: "hidden",
-        }}
-      >
-        <Image
-          source={require("../../assets/images/back.jpg")}
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            borderRadius: 12,
-          }}
-          resizeMode={"cover"}
-        />
+      <TouchableOpacity onPress={handlePressImages} activeOpacity={0.8}>
         <View
           style={{
             backgroundColor: "papayawhip",
             borderColor: "white",
             borderRadius: 12,
             borderWidth: 4,
-            margin: 12,
-            width: width / 3,
-            height: (width / 3) * 1.3,
+            margin: 8,
+            height: width * 1.3,
+            width: width - 16,
+            overflow: "hidden",
           }}
         >
           <Image
-            source={require("../../assets/images/front.jpg")}
+            source={srcs.mainSrc}
             style={{
               position: "absolute",
               width: "100%",
@@ -55,9 +56,33 @@ export const ActivityListItem = ({}: ActivityListItemProps) => {
             }}
             resizeMode={"cover"}
           />
+          <TouchableOpacity
+            onPress={handlePressImages}
+            activeOpacity={0.8}
+            style={{
+              backgroundColor: "papayawhip",
+              borderColor: "white",
+              borderRadius: 12,
+              borderWidth: 4,
+              margin: 12,
+              width: width / 3,
+              height: (width / 3) * 1.3,
+            }}
+          >
+            <Image
+              source={srcs.thumbnailSrc}
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                borderRadius: 12,
+              }}
+              resizeMode={"cover"}
+            />
+          </TouchableOpacity>
+          <EmojiPicker />
         </View>
-        <EmojiPicker />
-      </View>
+      </TouchableOpacity>
     </View>
   );
 };
